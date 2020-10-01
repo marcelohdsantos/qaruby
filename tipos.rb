294597e609874b9de306868dde635ca517e71718
Marcelo dos Santos <marcelodossantos732@yahoo.com.br>
2020-10-01 00:08:41 -0400

    first commit

---
+++ tipos.rb
@@ -0,0 +1,18 @@
+# -- tipos ruby --
+# o tipo de uma variável é definido no momento em que a mesma recebe o 
+# valor. É uma linguagem de tiágem dinâmica
+
+produto = 'macbook air'
+preco = 4500.99
+quantidade = 10
+disponivel = true
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+puts 'imprimir os valores das variáveis'
+puts produto
+puts preco
+puts quantidade
+puts disponivel
